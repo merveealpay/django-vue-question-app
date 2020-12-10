c66369147525b1894cec09dfec2280a16c509416
merveealpay <merveealpay@gmail.com>
2020-12-10 21:57:24 +0300

    answers, creation and listing

--- QuestionTime/questions/api/serializers.py
+++ QuestionTime/questions/api/serializers.py
@@ -13,7 +13,7 @@ class AnswerSerializer(serializers.ModelSerializer):
         exclude = ["question", "voters", "updated_at"]
 
     def get_created_at(self, instance):
-        return instance.created_at.strftime("%B %d %Y")
+        return instance.created_at.strftime("%B %d,%Y")
     
     def get_likes_count(self, instance):
         return instance.voters.count()
@@ -36,7 +36,7 @@ class QuestionSerializer(serializers.ModelSerializer):
 
 
     def get_created_at(self, instance):
-        return instance.created_at.strftime("%B %d %Y")
+        return instance.created_at.strftime("%B %d, %Y")
 
     def get_answers_count(self, instance):
         return instance.answers.count()
--- QuestionTime/questions/api/urls.py
+++ QuestionTime/questions/api/urls.py
@@ -1,11 +1,21 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+
 from questions.api import views as qv
 
 router = DefaultRouter()
 router.register(r'questions', qv.QuestionViewSet)
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+
+    path("questions/<slug:slug>/answer/",
+          qv.AnswerCreateAPIView.as_view(),
+          name="create-answer"),
+
+    path("questions/<slug:slug>/answers/",
+          qv.AnswerListAPIView.as_view(),
+          name="answer-list"),
+
 ]
