80822e5356651e5770ad506c94d85d584dfe7edd
merveealpay <merveealpay@gmail.com>
2020-12-27 01:49:18 +0300

    frontend

--- QuestionTime/frontend/src/App.vue
+++ QuestionTime/frontend/src/App.vue
@@ -1,33 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/example">Example</router-link>
-    </div>  
-  <router-view />
+    <NavbarComponent/>
+    <router-view/>
   </div>
 </template>
 
+<script>
+import { apiService } from "@/common/api.service.js";
+import NavbarComponent from "@/components/Navbar.vue";
+export default {
+  name: "App",
+  components: {
+    NavbarComponent
+  },
+  methods: {
+    async setUserInfo() {
+      // add the username of the logged in user to localStorage
+      const data = await apiService("/api/user/");
+      const requestUser = data["username"];
+      window.localStorage.setItem("username", requestUser);
+    }
+  },
+  created() {
+    this.setUserInfo()
+  }
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html,
+body {
+  height: 100%;
+  font-family: "Playfair Display", serif;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.btn:focus {
+  box-shadow: none !important;
 }
 </style>
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# django-vue-question-app
-DRF and Vuejs
+## Hot to set up the project to run on your local machine?
+
+#### Download the code to your PC, unpack the zip and move inside the folder.
+
+#### Create a new Python Virtual Environment:
+```
+python3 -m venv venv
+```
+
+#### Activate the environment and install all the Python/Django dependencies:
+
+```
+source ./venv/bin/activate
+pip install -m ./requirements.txt
+```
+
+#### Apply the migrations as usual.
+
+#### Time to install the Vue JS dependencies:
+```
+cd QuestionTime/frontend
+npm install
+```
+
+#### Run Vue JS Development Server:
+```
+npm run serve
+```
+
+#### Run Django's development server:
+```
+python manage.py runserver
+```
+
+#### Open up Chrome and go to 127.0.0.1:8000 and the app is now running in development mode!
